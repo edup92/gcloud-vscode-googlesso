72f2148b4b6637029719ed4dbf2385a9610e3ab1
edup92 <edup92mail@gmail.com>
2025-11-06 15:42:37 +0000

    up

--- variables.tf
+++ variables.tf
@@ -1,5 +1,3 @@
-
-
 variable "project_name" {
   description = "Project Name"
   type        = string
@@ -39,50 +37,6 @@ variable "oauth_client_id" {
 
 variable "oauth_client_secret" {
   description = "OAuth client secret for Google SSO authentication"
-  type        = string
-  sensitive   = true
-}
-
-variable "bw_installation_id" {
-  description = "Bitwarden installation ID in format XXXX-XXXX-XXXX"
-  type        = string
-  sensitive   = true
-}
-
-variable "bw_installation_key" {
-  description = "Bitwarden installation key"
-  type        = string
-  sensitive   = true
-}
-
-variable "bw_db_password" {
-  description = "Password for Bitwarden database"
-  type        = string
-  sensitive   = true
-}
-
-variable "bw_smtp_host" {
-  description = "SMTP server hostname for email delivery"
-  type        = string
-}
-
-variable "bw_smtp_port" {
-  description = "SMTP server port for email delivery"
-  type        = number
-}
-
-variable "bw_smtp_ssl" {
-  description = "Enable SSL/TLS for SMTP connection"
-  type        = bool
-}
-
-variable "bw_smtp_username" {
-  description = "SMTP server username for authentication"
-  type        = string
-}
-
-variable "bw_smtp_password" {
-  description = "SMTP server password for authentication"
   type        = string
   sensitive   = true
 }
